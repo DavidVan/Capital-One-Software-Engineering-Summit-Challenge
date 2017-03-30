d7b3ff74a81c00b1c51c09f7e999c26aaa34b23a
David Van <David@DavidVan.net>
2017-03-30 06:55:06 -0700

    Went back to simple-oauth2 after fixing issue with oauth not working under Linux. It was a line endings issue!

--- yelp.js
+++ yelp.js
@@ -1,42 +1,32 @@
 var fs = require('fs');
 var path = require('path');
-var https = require('https');
 
-var yelpClientID = fs.readFileSync(path.join(__dirname, 'YelpClientID'), 'utf8');
-var yelpClientSecret = fs.readFileSync(path.join(__dirname, 'YelpClientSecret'), 'utf8');
 
-var postString = 'grant_type=' + 'client_credentials' +
-                 '&client_id=' + yelpClientID +
-                 '&client_secret=' + yelpClientSecret;
-var options = {
-    hostname: 'api.yelp.com',
-    port: 443,
-    path: '/oauth2/token',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postString)
+// Read from file, or insert here. Your choice.
+var yelpClientID = fs.readFileSync(path.join(__dirname, 'YelpClientID'), 'utf8').replace(/\r?\n|\r/g,''); // Remove line endings.
+var yelpClientSecret = fs.readFileSync(path.join(__dirname, 'YelpClientSecret'), 'utf8').replace(/\r?\n|\r/g,''); // Remove line endings.
+
+var credentials = {
+    client: {
+        id: yelpClientID,
+        secret: yelpClientSecret
+    },
+    auth: {
+        tokenHost: 'https://api.yelp.com',
+        tokenPath: '/oauth2/token',
+        revokePath: '/oauth2/revoke'
     }
-}
+};
 
-var getAuth = function(callback) {
-    var req = https.request(options, function(res) {
-        res.setEncoding('utf8');
-        var json = [];
-        res.on('data', function (chunk) {
-            json.push(chunk);
-        });
-        res.on('end', function() {
-            callback(JSON.parse(json.join('')));
-        });
-    });
-
-    req.write(postString);
-    req.end();
-}
+var oauth2 = require('simple-oauth2').create(credentials);
+var tokenConfig = {};
 
 module.exports.getToken = function(callback) {
-    getAuth(function(auth) {
-        callback(auth.access_token); // Return the token for use
-    })
+    oauth2.clientCredentials.getToken(tokenConfig, function(error, result) {
+        if (error) {
+            return console.log('Access Token Error', error.message);
+        }
+
+        callback(oauth2.accessToken.create(result).token.access_token); // Return the token for use
+    });
 };
