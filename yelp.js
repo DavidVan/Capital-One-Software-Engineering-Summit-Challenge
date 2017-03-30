20a0dd81addb1ba8a8494fd32267a9833240ba92
David Van <David@DavidVan.net>
2017-03-30 00:37:43 -0700

    Added revoke path for API.

--- yelp.js
+++ yelp.js
@@ -10,7 +10,8 @@ var credentials = {
     },
     auth: {
         tokenHost: 'https://api.yelp.com',
-        tokenPath: '/oauth2/token'
+        tokenPath: '/oauth2/token',
+        revokePath: '/oauth2/revoke'
     }
 };
 
