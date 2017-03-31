307d45ef03c73a1800605f053feb835d2de4831c
David Van <David@DavidVan.net>
2017-03-31 01:08:27 -0700

    More bug fixes.

--- public/js/searchHelper.js
+++ public/js/searchHelper.js
@@ -59,7 +59,7 @@ var previousSearchTerm = '';
 var previousBusiness = '';
 
 var searchBox = document.getElementsByClassName('searchBox')[0];
-var searchButton = document.getElementsByClassName('submitButton')[0];
+var searchButton = document.getElementsByClassName('searchButton')[0];
 var searchResultsArea = document.getElementsByClassName('searchResults')[0];
 
 searchBox.addEventListener('input', function(e) {
@@ -125,31 +125,25 @@ searchButton.addEventListener('click', function(e) {
 function makeBusiness() {
     var randomIndex = Math.floor(Math.random() * (data.businesses.length - 0)) + 0;
     var randomBusiness = data.businesses[randomIndex];
-    if (randomBusiness.name.trim() === previousBusiness && data.business.length !== 1) {
-        while (randomBusiness.name.trim() === previousBusiness) {
+    if (randomBusiness.id.trim() === previousBusiness && data.total !== 1) {
+        while (randomBusiness.id.trim() === previousBusiness) {
             randomIndex = Math.floor(Math.random() * (data.businesses.length - 0)) + 0;
             randomBusiness = data.businesses[randomIndex];
         }
     }
     var name = randomBusiness.name.trim();
     var address = randomBusiness.location.display_address.join(',<br>').trim();
-    if (randomBusiness.location.display_address.length === 2) {
-        addressForMap = randomBusiness.location.display_address.join(',').trim();
-    }
-    else if (randomBusiness.location.display_address.length === 3) {
-        addressForMap = randomBusiness.location.display_address[0] + ',' + randomBusiness.location.display_address[2];
-    }
-    else {
-        addressForMap = randomBusiness.location.display_address[0];
-    }
+    addressForMap = randomBusiness.location.address1 + ',' + randomBusiness.location.zip_code + ',' + randomBusiness.location.country;
     var phone = randomBusiness.display_phone.trim();
-    previousBusiness = name;
+    previousBusiness = randomBusiness.id;
     var rating = randomBusiness.rating;
     var ratingImage = 'images/' + parseInt(rating) + 'star';
     if (!isInt(rating)) {
         ratingImage += 'half';
     }
     ratingImage += '.png';
+    var numberOfReviews = randomBusiness.review_count;
+    var price = randomBusiness.price;
     var source = '<div class="business">' +
                      '<div class="businessImage">' +
                          '<img src="' + randomBusiness.image_url + '"' + 'alt="' + randomBusiness.name + '">' +
@@ -159,7 +153,10 @@ function makeBusiness() {
                              '<p>' + address + '</p>' +
                              '<p>' + phone + '</p>' +
                              '<br>' +
-                             '<img src=' + ratingImage + ' alt=' + rating + ' stars' + '>'
+                             '<img src=' + ratingImage + ' alt=' + rating + ' stars' + '>' +
+                             '<br>' +
+                             '<p>Reviews: ' + numberOfReviews + '</p>' +
+                             '<p>Price: <span class="price">' + price + '</span></p>' +
                          '</div>' +
                      '</div>'
                  '</div>';
